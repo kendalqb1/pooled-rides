3ffa411d6def64a0c1b79e63c273450bc03aab32
Kendal Quesada <kendalqb1@gmail.com>
2025-03-09 20:37:07 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
-const nextConfig ={
-    output: 'export'
-  }
-  
-  module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',// This is required in order to Generate a static export
+    images: { unoptimized: true }, // This is required in order to Generate a static export
+};
+
+export default nextConfig;
